db31100e2eaee6eb4aa8906ee77052bd1b29403e
edwardedd <eddmos86@gmail.com>
2018-07-20 15:45:56 +0300

    Add edit, update, show and index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,44 @@
 class ArticlesController < ApplicationController
-	def new
-		@article = Article.new
-	end
+  def index
+    @articles = Article.all
+  end
 
-	def create
-		#render plain: params[:article].inspect
-		@article = Article.new(article_params)
-		@article.save
-		redirect_to articles_show(@article)
-	end
+  def new
+    @article = Article.new
+  end
 
-		private 
-		def article_params
-			params.require(:article).permit(:title, :description)
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-		end
+  def create
+    @article = Article.new(article_params)
+   if @article.save
+     flash[:notice] = 'article is successfully created'
+     redirect_to articles_path(@article)
+   else
+     render 'new'
+   end
+  end
 
+  def update
+   @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'articles updated'
+      redirect_to article.patch(@article)
+    else
+       render 'edit'
+    end
+ end
+
+  def show
+   @article = Article.find(params[:id])
+  end
+
+     private
+
+        def article_params
+          params.require(:article).permit(:title, :description)
+        end
 
 end
